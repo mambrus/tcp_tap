566d68859151bea3b876435f746b47e673df31e6
Michael Ambrus <michael.ambrus@gmail.com>
2011-02-28 09:31:05 +0100

    Some initial code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+
+/* Where to put intermediate files and pipes */
+char log_path[PATH_MAX]="/tmp/tcp_tap";
+
+/* stdin,stdout,stderr for the native process */
+char stdin_name[PATH_MAX]="stdin";
+char stdout_name[PATH_MAX]="stdout";
+char stderr_name[PATH_MAX]="stderr";
+
+/* Name of the main process to run */
+char stdout_err[PATH_MAX]="stderr";
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+  int myPipe[2];
+  
+  pipe(myPipe);
+
+  id (fork() == 0){
+	  /*Child*/
+  }
+
+  return 0;
 }
