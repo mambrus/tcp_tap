f4dec2ace9c2efed6fec0d29abade03b6afb0972
Michael Ambrus <michael.ambrus@gmail.com>
2011-03-02 17:40:04 +0100

    Almost done

--- server.c
+++ server.c
@@ -24,16 +24,28 @@ char port_number[PATH_MAX]="6666";
 
 #ifdef TEST
 int main(int argc, char **argv) {
-	int port;
+	int port,s,sfd;
 	char hostname[PATH_MAX];
 	struct hostent *hp;
+	struct sockaddr_in lsin, rsin;
 
 	port=atoi(port_number);
 	
 	assert(gethostname(hostname, sizeof(hostname) > 0));
 	sprintf(hostname,"%s","mambrus-laptop");
 
-	assert((hp = gethostbyname(hostname)) != NULL);
+	assert( (hp = gethostbyname(hostname)) != NULL );
+
+	assert( (s=socket(AF_INET, SOCK_STREAM,0)) >= 0 );
+	lsin.sin_family = AF_INET;
+	lsin.sin_port=htons(port);
+	memcpy(&lsin.sin_addr,hp->h_addr,hp->h_length);
+	assert(bind(s, &lsin, sizeof(lsin) ) >= 0);
+
+	assert(listen(s, 5) >= 0);
+
+	sfd = accept(s,&rsin
+	
 
 	printf("Hostname: [%s], Port: [%d]\n",hostname,port);
 	return 0;
