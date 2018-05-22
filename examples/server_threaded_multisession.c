fe9551c8680484ab015c156907e2b05c1e5d8dc4
Michael Ambrus <michael.ambrus@terranet.se>
2018-05-22 15:46:20 +0200

    GPL removed from examples

--- examples/server_threaded_multisession.c
+++ examples/server_threaded_multisession.c
@@ -1,22 +1,15 @@
-/***************************************************************************
- *   Copyright (C) 2011 by Michael Ambrus                                  *
- *   ambrmi09@gmail.com                                                    *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
+/* Echo service (multisession threaded)
+ * ====================================
+ *
+ * Wait for connect, then spawn a thread for each connection who will handle
+ * that particular session, then go back wait for more connections.
+ *
+ * Any TCP-client can be used.
+ *
+ * Example of client-side usage:
+ *
+ *   telnet localhost 6666
+ */
 #include <pthread.h>
 #include <limits.h>
 #include <unistd.h>
@@ -46,8 +39,6 @@ void *myThread(void *inarg)
     }
 }
 
-/* Wait for connect, then spawn a thread for each connection who will handle
- * that particualr session, then go back wait for more connections */
 int main(int argc, char **argv)
 {
     int fd, s;
