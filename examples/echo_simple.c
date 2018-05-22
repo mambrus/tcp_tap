aca4aa01eda35ef051fde7291280d3af3e27a972
Michael Ambrus <michael@helsinova.se>
2018-05-22 21:40:08 +0200

    More examples

---
+++ examples/echo_simple.c
@@ -0,0 +1,58 @@
+/* Echo service (single session)
+ * =============================
+ *
+ * Wait for connect
+ * Handle session until session terminates
+ * Repeat
+ *
+ * Any TCP-client can be used.
+ *
+ * Example of client-side usage:
+ *
+ *   telnet localhost 6688
+ */
+#include <limits.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <tcp-tap/server.h>
+
+#undef  NDEBUG
+#include <assert.h>
+
+#ifndef BUFF_SZ
+#define BUFF_SZ 0x400
+#endif
+
+#define PORT_NUMBER 6688
+#define HOST_IP "localhost"
+
+/* Handle session: Just echo back everything */
+int main(int argc, char **argv)
+{
+    int rn = BUFF_SZ, sn;
+    int fd,s;
+    char buf[BUFF_SZ];
+
+    s = init_server(PORT_NUMBER, "localhost");
+
+    while (1) {
+        printf("Pid=%d ready for echoing service: telnet %s %d\n",
+               getpid(), HOST_IP, PORT_NUMBER);
+        fd = open_server(s);
+        while (rn > 0) {
+            rn = read(fd, buf, BUFF_SZ);
+            sn = write(fd, buf, rn);
+            assert(rn == sn);
+        }
+        if (rn < 0) {
+            perror("read() failed: ");
+            close(fd);
+            exit(errno);
+        }
+    }
+
+    close(fd);
+    return 0;
+}
--- examples/switchboard_simple.c
+++ examples/switchboard_simple.c
@@ -3,16 +3,18 @@
  *
  * Shich-board shuffles all from all - to all
  *
+ * >>> This example is WIP <<<
+ *
  */
 
 #include <stdio.h>
-#include <pthread.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <tcp-tap/switchboard.h>
 #include <tcp-tap/server.h>
-#include "config.h"
+#include <stdlib.h>
+#include <errno.h>
 
 #undef  NDEBUG
 #include <assert.h>
@@ -22,59 +24,11 @@
 #define BUFF_SZ 0x400
 #endif
 
-#define PORT_NUMBER 1974
+#define PORT_NUMBER 1976
 #define HOST_IP "localhost"
-
-void *from_stdin(void *arg)
-{
-    int rn = BUFF_SZ, sn;
-    int wfd;
-    char buf[BUFF_SZ];
-
-    usleep(100000);
-    assert((wfd = open(Q_TO_SWTCH, O_WRONLY)) >= 0);
-
-    while (rn > 0) {
-        rn = read(1, buf, BUFF_SZ);
-        sn = write(wfd, buf, rn);
-        assert(rn == sn);
-    }
-
-    if (rn < 0)
-        perror("read() failed: ");
-    close(wfd);
-    return NULL;
-}
-
-void *to_stdout(void *arg)
-{
-    int rn = BUFF_SZ, sn;
-    int rfd;
-    char buf[BUFF_SZ];
-
-    usleep(100000);
-    assert((rfd = open(Q_FROM_SWTCH, O_RDONLY)) >= 0);
-
-    while (rn > 0) {
-        rn = read(rfd, buf, BUFF_SZ);
-        sn = write(2, buf, rn);
-        assert(rn == sn);
-    }
-
-    if (rn < 0)
-        perror("read() failed: ");
-
-    close(rfd);
-    return NULL;
-}
 
 int main(int argc, char **argv)
 {
-    pthread_t thread1, thread2;
-
-    assert(pthread_create(&thread1, NULL, from_stdin, NULL) == 0);
-    assert(pthread_create(&thread2, NULL, to_stdout, NULL) == 0);
-
     printf("Chat with me via stdin/stdout (I will echo) \n");
     printf("Others may join the group via: telnet %s %d\n", HOST_IP,
            PORT_NUMBER);
