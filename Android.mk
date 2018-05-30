08cec55d90dc92a54069f9ba39f908063eceabaf
Michael Ambrus <michael.ambrus@terranet.se>
2018-05-30 16:04:02 +0200

    BUILD_SHARED_LIBRARY build-rule added

--- Android.mk
+++ Android.mk
@@ -8,6 +8,13 @@ LOCAL_MODULE := libatcptap
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := debug eng
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+LOCAL_SRC_FILES :=  clientserver.c switchboard.c
+LOCAL_MODULE := libsotcptap
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := tcp-tap
 LOCAL_MODULE_TAGS := debug eng
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
